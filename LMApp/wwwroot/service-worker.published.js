d05132d06740beb5a536385bb459387f318fe0c8
Alexander Shakhov <samfromlv@gmail.com>
2025-08-06 01:41:43 +0300

    Fixed oauth callback

--- LMApp/wwwroot/service-worker.published.js
+++ LMApp/wwwroot/service-worker.published.js
@@ -78,7 +78,8 @@ async function onFetch(event) {
         // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
         const shouldServeIndexHtml = event.request.mode === 'navigate'
             && !event.request.url.startsWith(`${self.origin}/.auth`)
-            && !manifestUrlList.some(url => url === event.request.url);
+            && !event.request.url.startsWith(`${self.origin}/oauth-callback.html`)
+            && !manifestUrlList.some(url => url === event.request.url));
 
         const request = shouldServeIndexHtml ? 'index.html' : event.request;
         const cache = await caches.open(cacheName);
